160d10ec74d67740bb13cd1bfe134e301be70e83
00LT00 <lightning@zerokirin.online>
2021-03-14 05:22:10 +0800

    feat: fix swagger url
    Signed-off-by: 00LT00 <lightning@zerokirin.online>

--- app/internal/user/register.go
+++ app/internal/user/register.go
@@ -5,17 +5,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// 获取openID
-// @Tags user
-// @Summary get openID
-// @Description get openID from Weixin
-// @ID get-openID
-// @Produce  json
-// @Param code query string true "wx.login()获取的code"
-// @Param sign header string true "spppk"
-// @Success 200 {object} utils.SuccessResponse{data=string} "openID"
-// @Failure 500 {object} utils.FailureResponse "error request"
-// @Router /user [get]
 func OpenID(c *gin.Context) interface{} {
 	code := c.Query("code")
 	if code == "" {
